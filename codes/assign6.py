36b9182c89408f11d51f6f49de3ec69bdab50aa5
Manikanta0705 <102511255+Manikanta0705@users.noreply.github.com>
2022-05-09 03:19:31 +0530

    Create assign6.py

---
+++ codes/assign6.py
@@ -0,0 +1,8 @@
+#let P(A intersection B)= p_a_b
+#let P(B)=b
+#let P(A/B)= c
+import random
+p_a_b = random.random() #obtains random float between 0 and 1
+b = 0
+c = p_a_b/b
+print('Probability of event P(A/B):',c)
